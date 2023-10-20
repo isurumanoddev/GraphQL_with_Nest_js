c121765d56e6acd4e7b2ceba01869b1aa661a042
isurumanoddev <isurumanod99@gmail.com>
2023-10-21 04:46:09 +0530

    update

---
+++ src/employee/employee.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Employee } from "./entities/employee";
+import { FindOneOptions, Repository } from "typeorm";
+import { CreateEmployeeDto } from "./dto/create-employee.input";
+import { ProjectService } from "../project/project.service";
+import { Project } from "../project/entities/project";
+import { UpdateEmployeeInput } from "./dto/update-employee.input";
