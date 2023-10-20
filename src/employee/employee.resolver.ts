8b7dedb2baf45e951033e0ab282660584e9de343
isurumanoddev <isurumanod99@gmail.com>
2023-10-21 04:51:36 +0530

    update

--- src/employee/employee.resolver.ts
+++ src/employee/employee.resolver.ts
@@ -8,35 +8,5 @@ import { UpdateEmployeeInput } from "./dto/update-employee.input";
 @Resolver(() => Employee)
 export class EmployeeResolver {
 
-  constructor(private employeeService: EmployeeService) {
-  }
-
-  @Query(() => [Employee], { name: "getAllEmployee" })
-  findAll(): Promise<Employee[]> {
-    return this.employeeService.findAll();
-  }
-
-  @Query(() => Employee, { name: "getEmployeeById" })
-  findById(@Args("id", { type: () => Int }) id: number) {
-    return this.employeeService.findEmoloyeeById(id);
-  }
-
-  @Mutation(() => Employee, { name: "createEmployee" })
-  create(@Args("employee") employee: CreateEmployeeDto) {
-
-    return this.employeeService.create(employee);
-  }
-
-  @ResolveField(() => Project)
-  project(@Parent() employee: Employee) {
-    return this.employeeService.getProject(employee.projectId);
-  }
-
-  @Mutation(() => Employee, { name: "updateEmployee" })
-  updateEmployee(@Args("updateEmployeeInput") updateEmployeeInput: UpdateEmployeeInput) {
-    return this.employeeService.update(updateEmployeeInput.id,updateEmployeeInput)
-
-  }
-
 
 }
